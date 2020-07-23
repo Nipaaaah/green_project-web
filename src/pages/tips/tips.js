efbfc909e6ac5847f39c6ea5abd9836c04a5e975
Schwarzion <victor.lehouck@gmail.com>
2020-07-23 16:32:36 +0200

    Query all tips

--- src/pages/tips/tips.js
+++ src/pages/tips/tips.js
@@ -1,33 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
-import { getTips } from '../../services/tips.service'
+import { GetTips } from '../../services/tips.service'
 
-function Tips() {
-  const [tipsList, setListTips] = useState([]);
+// export default class Tips extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       tipList: []
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.getAllTips()
+//   }
+
+//   async getAllTips() {
+//     try {
+//       const res = await GetTips();
+//       setTimeout(() => {
+//         this.setState({ tipList: res.data.tips })
+//       }, 2000)
+//       console.log(this.state.tipList)
+//     }
+//     catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   render() {
+//     if (this.state.tipList.length === 0) {
+//       return (
+//         <div>
+//           <Container fluid>
+//             <Row>
+//               <Col>
+//                 Loading...
+//               </Col>
+//             </Row>
+//           </Container>
+//         </div >
+//       )
+//     }
+//     return (
+//       <div>
+//         <Container fluid>
+//           <Row>
+//             <Col>
+//               {this.state.tipList[0].id}
+//             </Col>
+//           </Row>
+//         </Container>
+//       </div >
+//     )
+//   }
+// }
+// const Tips = () => {
+//   const [tipList, setTipList] = useState([]);
+
+//   function getAllTips() {
+//     try {
+//       const res = GetTips();
+//       setTipList(res);
+//       console.log(tipList)
+//     }
+//     catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+
+const Tips = () => {
+
+  const [tipList, setTipList] = useState([])
 
   const getAllTips = async () => {
-    try {
-      const res = await getTips();
-      setListTips(res);
-    }
-    catch (error) {
-      console.log(error)
-    }
+    const res = await GetTips();
+    setTipList(res.data.tips);
+
   }
 
-  getAllTips()
-
-  return (
-    <div>
-      <Container fluid>
-        <Row>
-          <Col>
-            Hello Tips
-          </Col>
-        </Row>
-      </Container>
-    </div>
+  useEffect(() => {
+    setTimeout(
+      () => {
+        getAllTips();
+      }
+      , 1000)
+  },
+    [tipList],
   )
+
+  if (tipList.length === 0) {
+    return (
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              Loading...
+            </Col>
+          </Row>
+        </Container>
+      </div >
+    )
+  }
+  else {
+    return (
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              {tipList[0].name}
+            </Col>
+          </Row>
+        </Container>
+      </div >
+    )
+  }
 }
 
 export default Tips;
--- src/services/tips.service.js
+++ src/services/tips.service.js
@@ -1,13 +1,16 @@
 import axios from '../tools/axios'
 
-export const getTips = async () => {
+const GetTips = async () => {
 
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xOTIuMTY4LjAuMTY6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NTQ5ODIzNSwiZXhwIjoxNTk1NTAxODM1LCJuYmYiOjE1OTU0OTgyMzUsImp0aSI6IjkyclhtSTRQSDJnaGVyME0iLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.Meyufqlw1v_EdagmADhWpeVt5JSUL9jmiJxbLSCQgH8'
+  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xOTIuMTY4LjAuMTY6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NTUxMjg2MiwiZXhwIjoxNTk1NTE2NDYyLCJuYmYiOjE1OTU1MTI4NjIsImp0aSI6IkRQS04wa2tvSUJIeXFNbTQiLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.lCCMBbSFNdsS9One7cTZo_Oh2YYIlD3v9lXWWJok8VY'
   const config = {
     headers: { Authorization: `Bearer ${token}` }
   };
 
-  const result = await axios.get("/allTips", config);
-  console.log(result)
-  return result
+  let res = await axios.get("/allTips", config);
+  return res;
+}
+
+export {
+  GetTips
 }
