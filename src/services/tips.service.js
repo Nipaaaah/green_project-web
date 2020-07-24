f816b102574a80aafc6082c844cb60afe63bf7d2
Schwarzion <victor.lehouck@gmail.com>
2020-07-24 09:27:54 +0200

    added datatables

--- src/services/tips.service.js
+++ src/services/tips.service.js
@@ -2,12 +2,13 @@ import axios from '../tools/axios'
 
 const GetTips = async () => {
 
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xOTIuMTY4LjAuMTY6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NTUxMjg2MiwiZXhwIjoxNTk1NTE2NDYyLCJuYmYiOjE1OTU1MTI4NjIsImp0aSI6IkRQS04wa2tvSUJIeXFNbTQiLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.lCCMBbSFNdsS9One7cTZo_Oh2YYIlD3v9lXWWJok8VY'
+  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NTU3NDExOCwiZXhwIjoxNTk1NTc3NzE4LCJuYmYiOjE1OTU1NzQxMTgsImp0aSI6IktJaTVCRTVEMkxkTWwyUWgiLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.F5jzTK3-IbhYGD_Ljr6Ip-4f1fNE9aq9G-J34d0ahJA'
   const config = {
     headers: { Authorization: `Bearer ${token}` }
   };
 
   let res = await axios.get("/allTips", config);
+  console.log(res.data.tips)
   return res;
 }
 
