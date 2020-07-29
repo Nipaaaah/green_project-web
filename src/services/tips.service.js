5a245ddfa116771dadfd85dd66012dff9c27c714
Schwarzion <victor.lehouck@gmail.com>
2020-07-29 13:12:23 +0200

    [FEAT] EDIT

--- src/services/tips.service.js
+++ src/services/tips.service.js
@@ -1,14 +1,12 @@
 import axios from '../tools/axios'
 
-
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NTc4NDk5NywiZXhwIjoxNTk1Nzg4NTk3LCJuYmYiOjE1OTU3ODQ5OTcsImp0aSI6IktTVU1pcFNuT3k0Zm1XRXAiLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.SL0hNZMdT3Why5QPFGDB0G38kANGkRNALaLZIfxv4kQ'
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU5NjAxNzY1MywiZXhwIjoxNTk2MDIxMjUzLCJuYmYiOjE1OTYwMTc2NTMsImp0aSI6IlpZOUdoU2VjaXB3eVE3MGMiLCJzdWIiOjEsInBydiI6IjFiMmI3MzE0NzFhMjA0ZTRkYWY1MTRjYmJlYzNjZjhhNzk4OGU0YjMifQ.od-9BD4GwSBoEree2207X-rNuonhufr1g3mBuWrAdlg'
 const config = {
   headers: { Authorization: `Bearer ${token}` }
 };
 
 const GetTips = async () => {
   let res = await axios.get("/allTips", config);
-  console.log(res.data.tips)
   return res;
 }
 
@@ -17,12 +15,10 @@ const GetTip = async (id) => {
   return res;
 }
 
-const EditTip = async (formData) => {
-  console.log(formData)
-  // let res = await axios.post(`/editTip/${id}`, config);
-  // return res;
+const EditTip = async (id, formData) => {
+  let res = await axios.post(`/editTip/${id}`, formData, config);
+  return res;
 }
-
 
 const DeleteTip = async (id) => {
   let res = await axios.get(`/deleteTip/${id}`, config);
