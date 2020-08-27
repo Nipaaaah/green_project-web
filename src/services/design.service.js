b41f2293f15696e9209b87e8e4b3ee4292fab30d
Schwarzion <victor.lehouck@gmail.com>
2020-08-27 14:00:54 +0200

    Removing dist folder

--- src/services/design.service.js
+++ src/services/design.service.js
@@ -1,23 +1,22 @@
 const getStatusColor = (row) => {
-    if (row.tipStatus === 1) {
-      return "success"
-    }
-    else if (row.tipStatus === 0) {
-      return "danger"
-    }
+  if (row.tipStatus === 1) {
+    return "success"
   }
+  else if (row.tipStatus === 0) {
+    return "danger"
+  }
+}
 
-  const getStatusButtonText = (row) => {
-    if (row.tipStatus === 1) {
-      return "Enabled"
-    }
-    else if (row.tipStatus === 0) {
-      return "Disabled"
-    }
+const getStatusButtonText = (row) => {
+  if (row.tipStatus === 1) {
+    return "Enabled"
   }
-  
+  else if (row.tipStatus === 0) {
+    return "Disabled"
+  }
+}
 
 export {
-    getStatusButtonText,
-    getStatusColor
-  }
+  getStatusButtonText,
+  getStatusColor
+}
